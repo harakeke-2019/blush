2b4742e13b887c4fcf2a281dd01e2494995e0bac
Grant Rigby <grantrigby1992@gmail.com>
2019-02-08 14:25:13 +1300

    added plumbing feels api complete and accessible through react

--- client/api.js
+++ client/api.js
@@ -1,7 +1,11 @@
 import request from 'superagent'
-
+const url ='http://localhost:3000/api/v1/feelings'
 const pokeapiUrl = 'https://pokeapi.co/api/v2'
 
 export function getPokemons () {
   return request
+}
+
+export function getFeels () {
+  return request.get(url).then(res => res.body)
 }
