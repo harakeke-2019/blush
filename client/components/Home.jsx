01cb1ce72c3836becdcbba94e0593c6aae228378
RMH <riki@craftsmoothie.co.nz>
2019-02-08 14:22:24 +1300

    update-home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,35 +1,33 @@
-import React from "react"
+import React from 'react'
 
-class Home extends React.Component{
-  construct(props){
-    super()
-    this.state={}
+class Home extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
   }
-render(){
-  return(
-    <div className="pageWrapper">
-      <div className="logoWrapper">
+  render () {
+    return (
+      <div className="pageWrapper">
+        <div className="logoWrapper">
           <img src="pokemon-logo.png" alt="Logo" />
           <h3>Find your spirit-pokemon</h3>
-      </div>
-      <div className="buttonWrapper">
-        <form>
+        </div>
+        <div className="buttonWrapper">
+          <form>
             <input type="text" placeholder="  Full Name" /><br />
             <input type="text" placeholder="  Birthday" /><br />
             <input type="text" placeholder="  Location" /><br />
             <div className="enter">
-                <a href="dashboard.html">enter</a>
+              <a href="dashboard.html">enter</a>
             </div>
-        </form>
+          </form>
+
+        </div>
 
       </div>
 
-    </div>
-
-  )
+    )
+  }
 }
-
-}
-
 
 export default Home
